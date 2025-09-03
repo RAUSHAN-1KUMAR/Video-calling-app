4c01c6bc2d59e270b0ee023aead420e959f7d713
RAUSHAN-1KUMAR <raushankumar.dte@gmail.com>
2025-09-03 12:00:21 +0530

    reverting changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "http://localhost:5173",
     credentials: true, // allow frontend to send cookies
   })
 );
